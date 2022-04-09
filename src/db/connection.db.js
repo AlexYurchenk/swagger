1b610e5e53c2fdb424143aafabfa3f019126e8bf
Oleksii Yurchenko <yurchenkoalex13@gmail.com>
2022-04-09 21:21:45 +0300

    f

--- src/db/connection.db.js
+++ src/db/connection.db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-//const uri = process.env.URI_DB;
-const uri =
-    'mongodb+srv://AlexYurchenko:Anatomia123!@shop.obphg.mongodb.net/MyProject?authSource=admin&replicaSet=atlas-6yhzu7-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+const uri = process.env.URI_DB;
 
 const db = mongoose.connect(uri);
 
